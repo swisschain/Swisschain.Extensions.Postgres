ec4d5d8dce5d67b112f2c801153e1d2fb096f78c
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2020-07-23 00:37:08 +0300

    added ExceptionExtensions

---
+++ src/Swisschain.Extensions.Postgres/ExceptionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Npgsql;
+
+namespace Swisschain.Extensions.Postgres
+{
+    public static class ExceptionExtensions
+    {
+        public static bool IsPrimaryKeyViolationException(this PostgresException e)
+        {
+            const string constraintViolationErrorCode = "23505";
+            const string primaryKeyNamePrefix = "pk_";
+            const string primaryKeyNameSuffix = "_pkey";
+
+            return string.Equals(e.SqlState, constraintViolationErrorCode, StringComparison.InvariantCultureIgnoreCase) &&
+                   (e.ConstraintName.StartsWith(primaryKeyNamePrefix, StringComparison.InvariantCultureIgnoreCase) ||
+                    e.ConstraintName.EndsWith(primaryKeyNameSuffix, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public static bool IsUniqueConstraintViolationException(this PostgresException e, string constraintName)
+        {
+            const string constraintViolationErrorCode = "23505";
+
+            return string.Equals(e.SqlState, constraintViolationErrorCode, StringComparison.InvariantCultureIgnoreCase) &&
+                   string.Equals(e.ConstraintName, constraintName, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
